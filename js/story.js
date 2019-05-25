c77b215ea94499dfb9a098b63081caaa0b8892d0
Josiah Baumgartner <48170542+JosiahBaumgartner@users.noreply.github.com>
2019-05-24 21:17:42 -0700

    Update story.js
    fix img hopefully.  using shortened link

--- js/story.js
+++ js/story.js
@@ -11,7 +11,7 @@ function buildStory() {
   alert("All done! Here's your story.");
   var story = "There once was a man named " + name1 + " who was king of the " + ocupation1 + "s. He had " + noun1 + ", " + noun2 + " and " + noun3 + " beyond your wildest dreams. Before they hung him from the gallows, these were the final words he said: " + '"My ' + noun4 + " is yours for the taking, but you'll have to find it first. " +  "I left everything I own in " + place1 + '." Ever since, ' + ocupation1 + "s from all over the world set sail for " + place1 + " searching for the " + noun4 + ", the treasure that would make their dreams come true!"
   document.getElementById("main").innerHTML = story;
-  document.getElementById("bg.image").backgroundImage = (url("https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/b4978e7f-1e75-4f66-9e8f-524f5a96cc6c/d9nfsnx-64dbbf90-6141-44e3-bf87-5c966b4416f3.png/v1/fill/w_1192,h_670,q_70,strp/daily_sketch__7___one_piece__endless_ocean_by_kunokillerkarpfen_d9nfsnx-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTA4MCIsInBhdGgiOiJcL2ZcL2I0OTc4ZTdmLTFlNzUtNGY2Ni05ZThmLTUyNGY1YTk2Y2M2Y1wvZDluZnNueC02NGRiYmY5MC02MTQxLTQ0ZTMtYmY4Ny01Yzk2NmI0NDE2ZjMucG5nIiwid2lkdGgiOiI8PTE5MjAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.FS4-Y5HI4hibXtx-y5apOr23QotFc1R8xN7pUMHRfvY))
+  document.getElementById("bg.image").backgroundImage = ( "url('http://tny.im/iwF')" );
   return story;
 }
 
