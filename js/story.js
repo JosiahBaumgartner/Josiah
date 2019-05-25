a7e8023085b2dbfe98bd4dfa727aa785c7e1243f
Josiah Baumgartner <48170542+JosiahBaumgartner@users.noreply.github.com>
2019-05-24 19:55:17 -0700

    change JosiahLibs.html, added main div... added inner html to print within function

--- js/story.js
+++ js/story.js
@@ -10,6 +10,7 @@ const buildStory() {
   var place1 = prompt ("Please give me a place." + " [" + (questions -= 1) + " words left]")
   alert("All done! Here's your story.");
   var story = "There once was a man named " + name1 + " who was king of the " + ocupation1 + "s. He had " + noun1 + ", " + noun2 + " and " + noun3 + " beyond your wildest dreams. Before they hung him from the gallows, these were the final words he said: " + '"My ' + noun4 + " is yours for the taking, but you'll have to find it first. " +  "I left everything I own in " + place1 + '." Ever since, ' + ocupation1 + "s from all over the world set sail for " + place1 + " searching for the " + noun4 + ", the treasure that would make their dreams come true!"
+  document.getElementById("main").innerHTML = story;
 }
 
 document.getElementById("submit").addEventListener("click", buildStory());
