1e1ab91c10b0b8011bc119cb0be4f82745d5ad58
Josiah Baumgartner <48170542+JosiahBaumgartner@users.noreply.github.com>
2019-05-24 21:43:39 -0700

    bg img pathing
    updating bg img pathing, pulled from function and into html head

--- js/story.js
+++ js/story.js
@@ -10,7 +10,6 @@ function buildStory() {
   var place1 = document.getElementById("place1").value;
   var story = "There once was a man named " + name1 + " who was king of the " + ocupation1 + "s. He had " + noun1 + ", " + noun2 + " and " + noun3 + " beyond your wildest dreams. Before they hung him from the gallows, these were the final words he said: " + '"My ' + noun4 + " is yours for the taking, but you'll have to find it first. " +  "I left everything I own in " + place1 + '." Ever since, ' + ocupation1 + "s from all over the world set sail for " + place1 + " searching for the " + noun4 + ", the treasure that would make their dreams come true!"
   document.getElementById("main").innerHTML = story;
-  document.getElementById("bg.image").backgroundImage = ( url('https://img-9gag-fun.9cache.com/photo/aPY0xeB_460swp.webp') );
   return story;
 }
 
